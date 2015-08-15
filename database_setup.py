bc3c8a335a1e33bbb03dbb1d5ea9d7dba5245f09
naren <makernaren@gmail.com>
2015-08-15 13:28:18 +0530

    Add classes

--- database_setup.py
+++ database_setup.py
@@ -8,10 +8,19 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    pass
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+    pass
 
 
 
 
 
 
-engine = create_engine()
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
